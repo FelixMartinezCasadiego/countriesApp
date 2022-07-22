9373dd7cf4e69836014a01d0ad6f4419879df70b
FelixMartinezCasadiego <felix.martinezcasadiego@gmail.com>
2022-07-22 16:58:40 -0500

    search part 1

--- countries-app/src/components/pages/Search.jsx
+++ countries-app/src/components/pages/Search.jsx
@@ -3,6 +3,22 @@ import '../../style.css';
 
 export const Search = () => {
   return (
-    <div className='SearchColor'>Search</div>
+    <div className='searchMain'>
+    
+      <form className='formSearch'>
+        <input type="text" placeholder='Search for a country...' required />
+        <div className='btnSearch'>
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+      </form>
+
+      <div>
+        <div className='filterSearch'>
+          <p> Filter by Region </p>
+          <i class="fa-solid fa-angle-down"></i>
+        </div>
+      </div>
+
+    </div>
   )
 }
